7f04c4d7a5720e0bff4fde031676c76f880679fc
Eretz <90727764+EretzIsrael@users.noreply.github.com>
2023-08-01 18:12:33 -0300

    Add files via upload

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int inteiro; 
+    int n;
+    printf ("Digite um numero inteiro, positivo e diferente de zero:");
+        scanf ("%d", &inteiro);
+    n = (inteiro - 1) + 3;
+    printf ("Valor:");
+    scanf ("%d", &n);
+    
+
+}
