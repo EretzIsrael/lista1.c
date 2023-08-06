952c9332877b23aa2c6a1c5c2b75a42c45a90f4e
EretzIsrael <eretzisrael2022@gmail.com>
2023-08-06 17:47:15 -0300

    parte final e corrigida

--- ex5.c
+++ ex5.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 int main (){
     int inteiro;
-    int n;
-     printf ("Digite um numero inteiro, positivo e diferente de zero:");
+    int x;
+    int y;
+  
+    printf("Digite um numero inteiro, positivo e diferente de zero: ");
+    scanf ("%d", &inteiro);
+     x = inteiro - 1;
+     y = inteiro + 1;
+    printf ("A expressao n - 1 e igual a =  %d \n" ,x);
+    printf ("A expressao n + 1 e igual a = %d \n" ,y);
 
-         scanf ("%d", &inteiro);
+     
 
-     n = (inteiro - 1) + 3;
-
-     printf ("Valor:");
-
-     scanf ("%d", &n);
-
- }
+    
+}
