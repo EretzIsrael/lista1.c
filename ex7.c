3120d2912df74dd7a0e5be435f00e55ac6be2a24
EretzIsrael <eretzisrael2022@gmail.com>
2023-08-07 19:09:01 -0300

    atividade 7 feita!

--- ex7.c
+++ ex7.c
@@ -2,11 +2,11 @@
 int main (){
      int caminhao = 18;
      int alqueires = 250;
-     int viagens;
+     float viagens;
     printf ("Quantos caminhoes?:");
-    scanf ("%d", caminhao);
+    scanf ("%d", &caminhao);
     printf ("E quantos alqueires?");
-    scanf ("%d", alqueires);
+    scanf ("%d", &alqueires);
     viagens = alqueires / caminhao;
      printf ("Serao necessarias %.f viagens para transportar toda a colheita de laranjas", viagens);
 
