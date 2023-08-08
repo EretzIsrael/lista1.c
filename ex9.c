efc08ca22a645f902cfc096d0bb1ffb707044470
EretzIsrael <eretzisrael2022@gmail.com>
2023-08-07 22:07:56 -0300

    algo de errado no code

---
+++ ex9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*Desenvolva um programa que leia o nome e o preço de uma mercadoria.
+ O programa deverá calcular um aumento de 5% no preço da mercadoria e mostrar o nome da mercadoria e o seu novo preço.*/
+
+int main (){
+    // Declarando as Variáveis n -> nome e p -> preço
+    char nome; 
+    int p;
+    float novo;
+    //Input
+    printf("Nome do produto: ");
+    scanf ("%c", &nome);
+    printf ("Preço do produto: ");
+    scanf ("%p", &p);
+    //Calculation 
+    novo = p * 1.05;
+    printf("Preço novo: %.1f" , nome , novo);
+    return 0;
+}
+
